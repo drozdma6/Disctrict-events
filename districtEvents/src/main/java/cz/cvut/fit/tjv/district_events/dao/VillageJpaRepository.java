a074443224716179105df22a972deb73d505b7b5
drozdma6 <99978787+drozdma6@users.noreply.github.com>
2022-10-27 17:31:28 +0200

    Adding @Param

--- districtEvents/src/main/java/cz/cvut/fit/tjv/district_events/dao/VillageJpaRepository.java
+++ districtEvents/src/main/java/cz/cvut/fit/tjv/district_events/dao/VillageJpaRepository.java
@@ -3,10 +3,11 @@ package cz.cvut.fit.tjv.district_events.dao;
 import cz.cvut.fit.tjv.district_events.domain.Village;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VillageJpaRepository extends JpaRepository<Village, Long>{
     @Query(nativeQuery = true, value = "SELECT count(*)>0 FROM event_village_locations WHERE village_id = :village_id")
-    boolean isAnyEventInVillage(Long village_id);
+    boolean isAnyEventInVillage(@Param("village_id") Long village_id);
 }
